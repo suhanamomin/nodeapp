ab23f86e51ad6635a48150764bf453de0ec7a079
suhanamomin <121624362+suhanamomin@users.noreply.github.com>
2023-02-23 19:47:47 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-//importing node framework
-var express = require(‘express’);
- 
-var app = express();//Respond with "hello world" for requests that hit our root "/"
-app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
-});//listen to port 80 by default
+// Importing the Express framework
+var express = require('express');
+
+// Creating an Express application instance
+var app = express();
+
+// Setting up a route to handle GET requests to the root URL
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+// Setting up the server to listen on port 80
 app.listen(process.env.PORT || 80);
- 
+
+// Exporting the app for testing or reuse in other modules
 module.exports = app;
